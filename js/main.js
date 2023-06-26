b9221aa287de8a17592719988a56def07891f5ab
RightoRen <69438543+RightoRen@users.noreply.github.com>
2023-06-26 18:52:29 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,43 +1,66 @@
-//variables
+console.log("Javascript is connected");
+
+// always organize your java as variable function and event listener
+const puzzlePiecesDiv = document.querySelector(".puzzle-pieces");
 const theButtons = document.querySelectorAll("#buttonHolder img"),
-    puzzleBoard = document.querySelector(".puzzle-board"),
+    puzzleBoard = document.querySelector(".puzzle-board");
     puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+    puzzlePiece = document.querySelector(".puzzle-pieces"),
     dropZones = document.querySelectorAll(".drop-zone");
-//store the dragged piece in a global variable
-//we will need it in the handleDrop function    
-let draggedPiece;
+
+    let draggedPiece;
 
 function changeBGImage() {
-    //console.log("changeBGImage called");
-    //url('../images/backGround0.jpg');
-    puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`
+    resetButton();
+
+    puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`  
+};
+
+function resetButton() {
+
+    dropZones.forEach(dropZone => {
+
+    if(dropZone.children.length >= 1) {
+        puzzlePiecesDiv.appendChild(dropZone.children[0])
+    }
+    });
 }
 
+
 function handleStartDrag() {
-    //console.log("Started dragging this piece:", this)
+    // console.log("Started Dragging", this)
     draggedPiece = this;
 }
 
 function handleDragOver(e) {
     e.preventDefault();
-    //this will prevent the default dragover behaviour
-    //e is short for event, could be e, evt a well
+    // this will prevent the default dragover behaviour e is short for event, could be e,evt as well
     console.log("dragged over me");
 }
 
 function handleDrop(e) {
     e.preventDefault();
-    console.log("dropped something on me");
-    //this line moves the dragged piece from the left side of the board
-    //into whatever dropzone we choose.
+    console.log("Item has been dropped");
+    if(this.children.length>=1) {
+        return;
+    }
+
+    // this line adds the puzzle piece
     this.appendChild(draggedPiece);
 }
 
-//event Listeners
-theButtons.forEach(button => button.addEventListener("click", changeBGImage)); 
+
+
+theButtons.forEach(button => button.addEventListener("click", changeBGImage));
 
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
 
 dropZones.forEach(zone => zone.addEventListener("dragover", handleDragOver));
 
 dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
+
+
+
+
+
+
